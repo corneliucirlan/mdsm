5b7c1f4a29fb813ef0fe16041907e2d749343d76
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2020-12-15 14:59:45 +0200

    Get the latest zip from tags

--- src/js/demo/main.js
+++ src/js/demo/main.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Prism from 'prismjs'
 import Parser from 'html-react-parser'
 
@@ -7,10 +7,32 @@ import { install, use, configure } from './data'
 
 const Main = () => {
 
-	// Highlight code samples
+	// Set Github URL -- change later to proper URL
+	const gitURL = 'https://api.github.com/repos/corneliucirlan/mdsm/tags'
+
+	// Set the URL for the latest version
+	const [zipURL, setZipURL] = useState('https://github.com/corneliucirlan/mdsm/tags')
+
 	useEffect(() => {
+
+		// Get the latest Github release
+		getLatestGithubRelease()
+
+		// Highlight code samples
 		Prism.highlightAll()
-	})
+
+	}, []);
+
+	// Get the latest' release zip file
+	const getLatestGithubRelease = async () => {
+		const response = await fetch(gitURL)
+
+		// Update URL if status is OK
+		if (200 === response.status) {
+			const zips = await response.json()
+			setZipURL(zips[0].zipball_url)
+		}
+	}
 
 	return (
 		<main className='skewed-background'>
@@ -19,6 +41,7 @@ const Main = () => {
 				<section className='row'>
 					<div className='col-12'>
 						<h2 id='install'>{install.title}</h2>
+						<p>{install.description}</p>
 					</div>
 					<div className='col-12 col-md-6'>
 						<CodeSample lang='powershell' title={install.pm.title} code={install.pm.code} />
@@ -26,7 +49,7 @@ const Main = () => {
 					<div className='col-12 col-md-6'>
 						<h3>{install.manual.title}</h3>
 						<p>{Parser(install.manual.description)}</p>
-						<a href='#' className='btn btn-outline-primary'>{install.manual.buttonTitle}</a>
+						<a href={zipURL} target='_blank' className='btn btn-outline-primary'>{install.manual.buttonTitle}</a>
 					</div>
 				</section>
 
@@ -35,13 +58,14 @@ const Main = () => {
 						<h2 id='use'>{use.title}</h2>
 					</div>
 					<div className='col-12'>
-						{use.data.map((element, index) => <CodeSample key={index} lang={element.language} description={element.description} code={element.code} />)}
+						{use.data.map((element, index) => <CodeSample key={index} lang={element.language} description={Parser(element.description)} code={element.code} />)}
 					</div>
 				</section>
 
 				<section className='row'>
 					<div className='col-12'>
 						<h2 id='configure'>{configure.title}</h2>
+						<p>{configure.description}</p>
 					</div>
 					{configure.data.map((element, index) => 
 						<div key={index} className='col-12'>
